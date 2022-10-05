8424ad0cac0c762ea11cf920d590e66aefa74245
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-05 21:47:59 +0530

    PUT Data to DB

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 const port = 5000;
 
-// Read Data from MongoDB 
+// Read Data from MongoDB
 try {
   app.get("/", async (req, res) => {
     let data = await dbConnect();
@@ -16,12 +16,30 @@ try {
   console.log(err);
 }
 
+// Post Data to MongoDb
 try {
   app.post("/", async (req, res) => {
     let data = await dbConnect();
     let result = await data.insert(req.body);
     console.log(result);
     res.send(req.body);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+// Put Data From MongoDB
+try {
+  app.put("/:name", async (req, res) => {
+    let data = await dbConnect();
+    let result = await data.updateOne(
+      {
+        name: req.params.name,
+      },    
+      { $set: req.body }
+    );
+    console.log(req.body);
+    res.send(result);
   });
 } catch (err) {
   console.log(err);
